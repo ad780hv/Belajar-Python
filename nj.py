eef8d4a6f660151d6e3d34871c548fabde3b642a
Indah <50508071+ad780hv@users.noreply.github.com>
2021-04-21 23:31:09 +0700

    Delete nj.py

--- nj.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-
-try:
-	x = int(input("Masukan angka: "))
-	print("pemisah 50 dengan",x,"akan memberi kamu:", 50/x)
-except valueError:
-	print("input bukan bilangan bulat,silakan coba lagi....")
-	
